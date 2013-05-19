5d64c3fa107319b4942c1ff95b3f098a74a448b6
xhamir <xhamir.one@gmail.com>
2013-05-19 17:46:24 -0500

    Jair

---
+++ AndroidTabLayout/src/com/example/androidtablayout/ConfActivity.java
@@ -0,0 +1,10 @@
+package com.example.androidtablayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conf_layout);    }
+}
---
+++ AndroidTabLayout/src/com/example/androidtablayout/FavsActivity.java
@@ -0,0 +1,10 @@
+package com.example.androidtablayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FavsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.songs_layout);    }
+}
