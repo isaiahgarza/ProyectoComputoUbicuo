a2bf84731f16efdbf9e6c59c3a3e3a186cc4135c
Isaias Garza <ing.isaiasgarza@gmail.com>
2013-05-20 22:50:16 -0500

    Aspecto visual de tab conf terminado

--- AndroidTabLayout/src/com/example/androidtablayout/ConfActivity.java
+++ AndroidTabLayout/src/com/example/androidtablayout/ConfActivity.java
@@ -1,8 +1,10 @@
 package com.example.androidtablayout;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 
@@ -10,10 +12,53 @@ public class ConfActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.conf_layout);
+
+        TextView textViewChange = (TextView) findViewById(R.id.ConfTab_title);
+        textViewChange.setText(textViewChange.getText() + " " + getBTName());
+        textViewChange = (TextView) findViewById(R.id.ConfTab_mac);
+        textViewChange.setText(textViewChange.getText() + " " + getBTAddres());
     }
     
     public void onRadioButtonClick(View v){
     	RadioButton button = (RadioButton) v;
-    	Toast.makeText(ConfActivity.this, button.getText() + " was chosen.", Toast.LENGTH_SHORT).show();
+    	int buttonID = button.getId();
+    	String chos = "";
+    	switch(buttonID){
+    	case R.id.radio_voice:
+    		chos = "VOICE";
+    		break;
+    	case R.id.radio_tone:
+    		chos = "TONE";
+    		break;
+    	case R.id.radio_song:
+    		chos = "SONG";
+    		break;
+    	case R.id.radio_vibr:
+    		chos = "VIBRATION";
+    		break;
+    	default:
+    		chos = "ERR";
+    		break;
+    	}
+    	
+    	//Toast.makeText(ConfActivity.this, button.getText() + " was chosen.", Toast.LENGTH_SHORT).show();
+    	Toast.makeText(ConfActivity.this, chos + " saved to configuration file", Toast.LENGTH_SHORT).show();
     }
+    
+    public String getBTName(){
+    	android.bluetooth.BluetoothAdapter BTadapter = null;
+    	if(BTadapter == null){
+    		BTadapter = BluetoothAdapter.getDefaultAdapter();
+    	}
+    	String name = BTadapter.getName();
+    	return name;
+    }
+    
+    public String getBTAddres(){
+    	BluetoothAdapter BTadapter = BluetoothAdapter.getDefaultAdapter();
+    	System.out.println(BTadapter.getAddress());
+    	return BTadapter.getAddress();
+    }
+    
+    public void saveAlertPreference(){;}
 }
